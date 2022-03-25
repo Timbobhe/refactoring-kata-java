5677849f5888158c79f1844a18f3d4210560c82d
diallom2 <mamadoualiou.diallo.ext@ag2rlamondiale.fr>
2022-03-25 08:29:05 +0100

    0000 - TU

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -1,21 +1,26 @@
 package com.sipios.refactoring.controller;
 
 import com.sipios.refactoring.UnitTest;
+import com.sipios.refactoring.business.IShoppingFacade;
 import com.sipios.refactoring.business.domain.Body;
 import com.sipios.refactoring.business.domain.Item;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 class ShoppingControllerTests extends UnitTest {
 
     @InjectMocks
     private ShoppingController controller;
 
+    @Mock
+    private IShoppingFacade shoppingFacade;
+
     @Test
     void should_not_throw() {
         Assertions.assertDoesNotThrow(
-            () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
+            () -> controller.getPrice(new Body(new Item[]{}, "STANDARD_CUSTOMER"))
         );
     }
 }
